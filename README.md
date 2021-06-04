09fa96e80defef25f6bc4f1dedfe2f9ec8a71df0
ErinCleaver-GRCC <erincleaver@email.grcc.edu>
2021-06-03 20:54:46 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# caclulator-6-3-2021
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/caclulator-6-3-2021)
---
+++ index.js
@@ -0,0 +1,28 @@
+class Calulator {
+
+  constructor(name) {
+    this.name = name
+    this.result = 0;
+  }
+
+  add(num) {
+    // summation code
+  }
+
+  subtract(num) {
+    // subtraction code
+  }
+
+  clear() {
+    //clear result
+  }
+
+  print() {
+    console.log(this.result)
+  }
+}
+
+// delcare and instantiate class
+
+
+// have fun with the calculator
